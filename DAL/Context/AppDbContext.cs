746ca714fdea543580e3fd60604e4ce83d68d2e1
Christina Golovach <christina.golovach@gmail.com>
2019-03-03 23:13:04 -0800

    Add initial commit

---
+++ DAL/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using DAL.Configuration;
+using DAL.Entities;
+
+namespace DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("InternationWidgets")
+        {
+            
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //TODO: setup dbcontext configuration using EF Fluent API. DO NOT add property attributes for any files from "Entities" folder.
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
